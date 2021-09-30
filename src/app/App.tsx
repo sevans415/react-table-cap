634392c8a285ed1b57d995a0698614a3bce134cc
Spencer Evans <sevans415@gmail.com>
2021-09-30 00:11:44 -0400

    mouse tracking set up. cropping established

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,56 @@
-import React from "react";
+import React, { useState, MouseEvent } from "react";
 
-const App = () => {
-  const takeScreenshotAndPreview = () => {
+const tempCropArea = {
+  x: 0,
+  y: 0,
+  width: 2000,
+  height: 2000
+};
+
+const takeScreenshotAndPreview = () => {
+  // @ts-ignore
+  window.electronBridge.takeScreenshot(
+    tempCropArea,
     // @ts-ignore
-    window.electronBridge.takeScreenshot(base64data => {
+    base64data => {
       // Draw image in the img tag
       document.getElementById("my-preview").setAttribute("src", base64data);
-    }, "image/png");
+    },
+    "image/png"
+  );
+};
+
+const App = () => {
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [currMousePosition, setCurrMousePosition] =
+    useState<null | { x: number; y: number }>(null);
+  const [downMousePosition, setDownMousePosition] =
+    useState<null | { x: number; y: number }>(null);
+
+  const mouseDown = (e: MouseEvent) => {
+    setDownMousePosition({ x: e.screenX, y: e.screenY });
+    setIsMouseDown(true);
+    console.log(e);
   };
+
+  const mouseUp = (e: MouseEvent) => {
+    console.log(e);
+    setIsMouseDown(false);
+    takeScreenshotAndPreview();
+  };
+
+  const mouseMove = (e: MouseEvent) => {
+    setCurrMousePosition({ x: e.screenX, y: e.screenY });
+    console.log(e);
+  };
+
   return (
-    <div>
+    <div
+      onMouseDown={mouseDown}
+      onMouseUp={mouseUp}
+      onMouseMove={isMouseDown ? mouseMove : undefined}
+      style={{ width: "100vw", height: "100vh" }}
+    >
       <h2>Hello from React!</h2>
       <button id="trigger" onClick={takeScreenshotAndPreview}>
         Take Screenshot
--- src/preload.ts
+++ src/preload.ts
@@ -1,14 +1,16 @@
-import { contextBridge, desktopCapturer } from "electron";
+import { contextBridge, desktopCapturer, Rectangle } from "electron";
 
-const takeScreenshot = (callback: (img: string) => void): void => {
+const takeScreenshot = (
+  cropArea: Rectangle,
+  callback: (img: string) => void
+): void => {
   desktopCapturer
     .getSources({
       types: ["screen"],
       thumbnailSize: { height: 2000, width: 2000 }
     })
     .then(sources => {
-      const img = sources[0].thumbnail.toDataURL();
-      console.log(img);
+      const img = sources[0].thumbnail.crop(cropArea).toDataURL();
       callback(
         img // The image to display the screenshot
       );
