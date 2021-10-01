aaac34ec7b94fa2c85cb4c84cb2c4829501f35a2
Spencer Evans <sevans415@gmail.com>
2021-09-30 20:16:56 -0400

    cropping image successfully

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,33 @@
+import { Rectangle } from "electron";
 import React, { useState, MouseEvent } from "react";
-
-const tempCropArea = {
-  x: 0,
-  y: 0,
-  width: 2000,
-  height: 2000
-};
 
 interface Point {
   x: number;
   y: number;
 }
 
-const takeScreenshotAndPreview = () => {
+const takeScreenshotAndPreview = async (cropArea: Rectangle) => {
   // @ts-ignore
-  window.electronBridge.takeScreenshot(
-    tempCropArea,
-    // @ts-ignore
-    base64data => {
-      // Draw image in the img tag
-      document.getElementById("my-preview").setAttribute("src", base64data);
-    },
-    "image/png"
-  );
+  window.electronBridge.invokeTakeScreenshot(cropArea).then(base64data => {
+    // Draw image in the img tag
+    document
+      .getElementsByClassName("img-preview")[0]
+      .setAttribute("src", base64data);
+  });
 };
+
+const TEMP_Y_ADJUSTER = -5;
+
+const getRectDimensions = (
+  pt1: Point,
+  pt2: Point,
+  adjust: Point = { x: 0, y: 0 }
+): Rectangle => ({
+  x: Math.min(pt1.x, pt2.x) + adjust.x,
+  y: Math.min(pt1.y, pt2.y) + adjust.y,
+  width: Math.abs(pt1.x - pt2.x),
+  height: Math.abs(pt1.y - pt2.y)
+});
 
 const App = () => {
   const [isMouseDown, setIsMouseDown] = useState(false);
@@ -31,24 +35,33 @@ const App = () => {
     useState<null | Point>(null);
   const [downMousePosition, setDownMousePosition] =
     useState<null | Point>(null);
+  const [screenDimensionDiff, setScreenDimensionDiff] =
+    useState<null | Point>(null);
 
   const mouseDown = (e: MouseEvent) => {
-    setDownMousePosition({ x: e.clientX, y: e.clientY });
+    const { clientX, clientY, screenX, screenY } = e;
+    setScreenDimensionDiff({ x: screenX - clientX, y: screenY - clientY });
+    setDownMousePosition({ x: clientX, y: clientY });
     setIsMouseDown(true);
     console.log(e);
-    console.log("DOWN:", { x: e.clientX, y: e.clientY });
+    console.log("DOWN:", { x: clientX, y: clientY });
     e.preventDefault();
   };
 
   const mouseUp = (e: MouseEvent) => {
     // console.log(e);
     console.log("up:", { x: e.clientX, y: e.clientY });
+    takeScreenshotAndPreview(
+      getRectDimensions(downMousePosition, currMousePosition, {
+        ...screenDimensionDiff,
+        y: screenDimensionDiff.y + TEMP_Y_ADJUSTER
+      })
+    );
+    console.log("MOUSE UP");
+    console.log("down ", downMousePosition, "\n up: ", currMousePosition);
     setIsMouseDown(false);
     setCurrMousePosition(null);
     setDownMousePosition(null);
-    takeScreenshotAndPreview();
-    console.log("MOUSE UP");
-    console.log("down ", downMousePosition, "\n up: ", currMousePosition);
   };
 
   const mouseMove = (e: MouseEvent) => {
@@ -57,16 +70,18 @@ const App = () => {
     }
   };
 
-  const insideScreenshotStyles =
+  const currentRect =
     downMousePosition && currMousePosition
-      ? {
-          // TODO: need to adjust for if they go right -> left, or down -> up
-          top: downMousePosition.y,
-          left: downMousePosition.x,
-          width: Math.abs(currMousePosition.x - downMousePosition.x),
-          height: Math.abs(currMousePosition.y - downMousePosition.y)
-        }
+      ? getRectDimensions(downMousePosition, currMousePosition)
       : null;
+
+  const insideScreenshotStyles = currentRect
+    ? {
+        ...currentRect,
+        top: currentRect.y,
+        left: currentRect.x
+      }
+    : null;
 
   return (
     <div
@@ -79,7 +94,10 @@ const App = () => {
         <div className="inside-screenshot" style={insideScreenshotStyles}></div>
       )}
 
-      <img id="my-preview" style={{ width: "90%" }} />
+      <img
+        className="img-preview"
+        style={{ width: "90%", alignContent: "center" }}
+      />
     </div>
   );
 };
--- src/preload.ts
+++ src/preload.ts
@@ -1,22 +1,7 @@
-import { contextBridge, desktopCapturer, Rectangle } from "electron";
-
-const takeScreenshot = (
-  cropArea: Rectangle,
-  callback: (img: string) => void
-): void => {
-  desktopCapturer
-    .getSources({
-      types: ["screen"],
-      thumbnailSize: { height: 2000, width: 2000 }
-    })
-    .then(sources => {
-      const img = sources[0].thumbnail.crop(cropArea).toDataURL();
-      callback(
-        img // The image to display the screenshot
-      );
-    });
-};
+import { contextBridge, Rectangle, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("electronBridge", {
-  takeScreenshot
+  // takeScreenshot,
+  invokeTakeScreenshot: (cropArea: Rectangle) =>
+    ipcRenderer.invoke("take-screenshot", cropArea)
 });
